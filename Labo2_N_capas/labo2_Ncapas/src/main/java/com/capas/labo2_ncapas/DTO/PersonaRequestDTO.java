a5d48e361c0f40c8b353db00bdb219b7a2d140f0
David Alejandro Sandoval Quijano <00379422@uca.edu.sv>
2026-05-23 20:49:53 -0600

    :zap: feat: adding pattern

--- Labo2_N_capas/labo2_Ncapas/src/main/java/com/capas/labo2_ncapas/DTO/PersonaRequestDTO.java
+++ Labo2_N_capas/labo2_Ncapas/src/main/java/com/capas/labo2_ncapas/DTO/PersonaRequestDTO.java
@@ -2,24 +2,30 @@ package com.capas.labo2_ncapas.DTO;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class PersonaRequestDTO {
 
     @NotBlank(message = "El nombre es obligatorio")
+    @Pattern(regexp = "^[a-zA-ZáéíóúÁÉÍÓÚñÑ\\s]+$", message = "El nombre solo puede contener letras")
     private String nombre;
 
     @NotBlank(message = "El DUI es obligatorio")
+    @Pattern(regexp = "^\\d{8}-\\d$", message = "El DUI debe tener el formato 00000000-0")
     private String dui;
 
     @NotBlank(message = "El teléfono es obligatorio")
+    @Pattern(regexp = "^\\d{4}-\\d{4}$", message = "El teléfono debe tener el formato 0000-0000")
     private String telefono;
 
     @NotBlank(message = "La calle es obligatoria")
+    @Pattern(regexp = "^[a-zA-ZáéíóúÁÉÍÓÚñÑ\\s0-9]+$", message = "La calle solo puede contener letras y números")
     private String calle;
 
     @NotBlank(message = "La colonia es obligatoria")
+    @Pattern(regexp = "^[a-zA-ZáéíóúÁÉÍÓÚñÑ\\s0-9]+$", message = "La colonia solo puede contener letras y números")
     private String colonia;
 
     @NotNull(message = "La zona es obligatoria")
