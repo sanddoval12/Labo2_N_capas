241ed9c7d1c627256fd49bf063085ea35281cfbb
David Alejandro Sandoval Quijano <00379422@uca.edu.sv>
2026-05-23 20:47:44 -0600

    :zap: feat: improving exception

--- Labo2_N_capas/labo2_Ncapas/src/main/java/com/capas/labo2_ncapas/Exception/GlobalExceptionHandler.java
+++ Labo2_N_capas/labo2_Ncapas/src/main/java/com/capas/labo2_ncapas/Exception/GlobalExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestControllerAdvice
@@ -29,6 +30,34 @@ public class GlobalExceptionHandler {
         error.put("status", 400);
         error.put("error", "Solicitud inválida");
         error.put("message", ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+
+    }
+
+    @ExceptionHandler(org.springframework.web.bind.MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidation(org.springframework.web.bind.MethodArgumentNotValidException ex) {
+        Map<String, Object> error = new HashMap<>();
+        error.put("timestamp", LocalDateTime.now());
+        error.put("status", 400);
+        error.put("error", "Datos inválidos");
+
+        List<String> mensajes = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(field -> field.getDefaultMessage())
+                .toList();
+
+        error.put("messages", mensajes);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
+
+    @ExceptionHandler(org.springframework.dao.DataIntegrityViolationException.class)
+    public ResponseEntity<Map<String, Object>> handleDataIntegrity(org.springframework.dao.DataIntegrityViolationException ex) {
+        Map<String, Object> error = new HashMap<>();
+        error.put("timestamp", LocalDateTime.now());
+        error.put("status", 400);
+        error.put("error", "Datos duplicados");
+        error.put("message", "Ya existe un registro con esos datos, verifique el DUI u otros campos únicos");
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 
